8e513dd5428bdd228e4d594f9484187e96b420a6
oleeo-recruit <darren.sutton@oleeo.com>
2025-12-24 00:21:39 +0000

    Add files via upload

---
+++ js/4fmC5VnrDxr3.js
@@ -0,0 +1,161 @@
+﻿$(function () {
+    if (shouldDisplayPopup()) {
+        $("#popup_overlay").popup({
+            transition: "all 0.3s",
+            autoopen: true,
+            scrolllock: true,
+            onclose: function () {
+                setCookie("emailme-do-not-show-popup");
+            }
+        });
+    }
+    if (shouldDisplayModal()) {
+        $("#popup_modal").popup({
+            transition: "all 0.3s",
+            autoopen: true,
+            scrolllock: true,
+            onclose: function () {
+                setCookie("hide-modal");
+            }
+        });
+    }
+
+    if (displaySuccessMessage()) {
+        $('#overlay_content').addClass('hide');
+        $('#successMessage').removeClass("hide").addClass('alert alert-success');
+        $('#overlay_close').removeClass("hide").addClass('btn btn-primary pull-right');
+        $('#successMessage').html("Thank you for signing up, you will begin receiving updates soon.");
+        $('#trigger-emailme-popup').remove;
+    }
+    else if (displayErrorMessage()) {
+        $('#privacyMessage').removeClass("hide").addClass('alert alert-danger');
+        $('#overlay_close').removeClass("hide").addClass('btn btn-primary pull-right');
+        $('#privacyMessage').html("Sorry there has been an error and we haven't been able to sign you up. Please try again.");
+    }
+
+    $(".beta_emailme").click(function (e) {
+        e.preventDefault();
+        setCookie("emailme-do-not-show-popup", 365);
+        window.location = "//www.nottinghamshire.gov.uk/emailme";
+    });
+
+    $("#formSubmit").click(function (e) {
+        if ($('#email').val().length === 0) {
+            $('#email').addClass('alert-danger');
+            e.preventDefault();
+        }
+        if (!$('#privacy').is(":checked")) {
+            $('#privacyMessage').removeClass("hide").addClass('alert alert-danger');
+            $('#privacyMessage').html("To sign up you must enter your email address and check the box to confirm that you consent to our data privacy policy");
+            e.preventDefault();
+        }
+        if ($('#privacy').is(":checked") && $('#email').val().length > 0) {
+            displaySuccessMessage();
+        }
+    });
+
+    $("#signUp").click(function (e) {
+        if ($('#email').val().length === 0) {
+            $('#email').addClass('alert-danger');
+            $('#privacyMessage').removeClass("hide").addClass('alert alert-danger');
+            $('#privacyMessage').html("To sign up you must enter your email address");
+            e.preventDefault();
+        }
+
+        if ($('#email').val().length > 0) {
+            $("#popup_overlay").popup("hide");
+            setCookie("emailme-do-not-show-popup", 365);
+        }
+    });
+
+    $(".beta_block").click(function (e) {
+        e.preventDefault();
+        $("#popup_overlay").popup("hide");
+        setCookie("emailme-do-not-show-popup", 365);
+    });
+
+    $(".beta_delay").click(function (e) {
+        e.preventDefault();
+        $("#popup_overlay").popup("hide");
+        setCookie("emailme-do-not-show-popup");
+    });
+
+    $(".beta_info_close").click(function (e) {
+        $("#popup_overlay").popup("hide");
+    });
+
+    $("#overlay_close").click(function (e) {
+        $("#popup_overlay").popup("hide");
+        setCookie("emailme-do-not-show-popup", 365);
+    });
+
+    $(".modal_block").click(function (e) {
+        e.preventDefault();
+        $("#popup_modal").popup("hide");
+        setCookie("hide-modal", 365);
+    });
+
+    $(".modal_delay").click(function (e) {
+        e.preventDefault();
+        $("#popup_modal").popup("hide");
+        setCookie("hide-modal");
+    });
+});
+
+function setCookie(cookieName, duration) {
+    $.cookie(cookieName, "true", { expires: duration });
+    console.log($.cookie());
+}
+
+function clearCookie(cookieName) {
+    $.removeCookie(cookieName, { path: "/" });
+}
+
+function shouldDisplayPopup() {
+    var cookie = $.cookie("emailme-do-not-show-popup");
+    var trigger = $("#trigger-emailme-popup");
+    //if ($.cookie("emailme-do-not-show-popup")) {
+    //	console.log("Cookie path is: " + $.cookie("emailme-do-not-show-popup"));
+    //} else {
+    //	console.log("no cookie!");
+    //}
+    //console.log("Location path is: " + window.location.pathname);
+    if ((cookie === undefined) && trigger.length > 0) {
+        return true;
+   }
+
+    return false;
+}
+function shouldDisplayModal() {
+    var cookie = $.cookie("hide-modal");
+    var trigger = $("#trigger-modal");
+    //if ($.cookie("hide-modal")) {
+    //	console.log("Cookie path is: " + $.cookie("hide-modal"));
+    //} else {
+    //	console.log("no cookie!");
+    //}
+    //console.log("Location path is: " + window.location.pathname);
+    if ((cookie === undefined) && trigger.length > 0) {
+        return true;
+    }
+
+    return false;
+}
+function displaySuccessMessage() {
+    var cookie = $.cookie("emailme-do-not-show-popup");
+    var signedUp = $.cookie("emailme");
+    if (cookie === undefined && signedUp === "autosigned=OK") {
+        return true;
+    }
+
+    return false;
+}
+
+function displayErrorMessage() {
+    var cookie = $.cookie("emailme-do-not-show-popup");
+    var signedUp = $.cookie("emailme");
+    if (cookie === undefined && (signedUp !== undefined && signedUp.length > 13)) {
+        return true;
+    }
+    return false;
+}
---
+++ js/W2iEWQoeZDl1.js
@@ -0,0 +1,123 @@
+﻿$(function ($) {
+
+    //Global cookie acceptance banner
+
+    var cookieMessage = document.getElementById('cookie-message')
+    var cookieAcceptBtn = document.getElementById('accept-cookies');
+    if (document.cookie.indexOf('acceptCookies=1') === -1) {
+        cookieMessage.style.maxHeight = cookieMessage.scrollHeight + 60 + "px";;
+		var acceptCookies = function () {
+			cookieMessage.style.maxHeight = null;
+			document.cookie = "acceptCookies=1;path=/;max-age=31536000";
+		};
+        cookieAcceptBtn.addEventListener('click', acceptCookies);
+    }
+
+    //Global cookie acceptance banner end
+
+	var $table = $('#councillor-table');
+
+	$(".show_more_services").on("click", function (e) {
+		e.preventDefault();
+		$(".all-services").toggleClass("d-none");
+		$(this).toggleClass("button-active");
+		$(".trending_tasks_filter").toggleClass("filter-show");
+		$("#task_filter").focus().toggleClass("filter-input-active");
+		$(".show_more_services_inactive").toggleClass("visually-hidden");
+		$(".show_more_services_active").toggleClass("d-block");
+	});
+
+	$(".show_steps").on("click", function (e) {
+		$(".show_steps_icon").toggleClass("hide");
+		$(".steps").toggle();
+	});
+
+	$(".switcher_browse, .switcher_tasks").on("click", function (e) {
+		e.preventDefault();
+		if ($(this).hasClass("active")) {
+		}
+		else {
+			$(".trending_tasks_box").toggleClass("hide");
+			$(".browse_box").toggleClass("hide");
+			$(".switcher_browse, .switcher_tasks").toggleClass("active");
+		}
+	});
+
+	$(".transaction-requirement").on("click", function () {
+		$(this).children(".checked").toggleClass("hide");
+		$(this).children(".unchecked").toggleClass("hide");
+	});
+
+	$(function () {
+		var hash = window.location.hash;
+		if (hash === "#browsehome") {
+			$(".trending_tasks_box").toggleClass("hide");
+			$(".browse_box").toggleClass("hide");
+			$(".switcher_browse, .switcher_tasks").toggleClass("active");
+		}
+	});
+
+	$(function () {
+		$('#task_filter').fastLiveFilter('.trending_tasks', {
+			callback: function(totalTasks) {
+				totalTasks === 1 ? $('#numTasks').html(totalTasks + " task") : $('#numTasks').html(totalTasks + " tasks");
+			}
+		});
+	});
+
+	$(".content").fitVids();
+
+	//Adding noreferrer to any link to a Firmstep form or to consultation or jobs so that Google Analytics will see the journey as one visit and can be tracked
+    $('[href*="forms.nottinghamshire.gov.uk"]').attr('rel', 'noreferrer');
+    $('[href*="consult.nottinghamshire.gov.uk"]').attr('rel', 'noreferrer');
+    $('[href*="nottinghamshire.tal.net"]').attr('rel', 'noreferrer');	
+
+	//Image gallery macro - uses glide.min.js plugin
+	var galleries = document.getElementsByClassName('image-gallery');
+
+	function imageGallerySetup() {
+	  var glide = galleries[i].querySelector('.glide'),
+		  overlay = galleries[i].querySelector('.gallery-overlay'),
+		  closeButton = galleries[i].querySelector('.gallery-overlay-close'),
+		  slideshow = new Glide(glide, {
+			type: 'carousel',
+			animationDuration: 1000,
+			dragThreshold: false,
+			peek: 400,
+			breakpoints: {
+			  1500: {
+				peek: 200
+			  },
+			  1200: {
+				peek: 150
+			  },
+			  992: {
+				peek: 100
+			  },
+			  768: {
+				peek: 50
+			  },
+			  576: {
+				peek: 25,
+				gap: 5
+			  }
+			}
+		  }).mount();
+	  galleries[i].addEventListener('click', function (e) {
+		e.preventDefault();
+		if (e.target.tagName !== 'IMG') return;
+		overlay.style.display = 'flex';
+		slideshow.update({
+		  startAt: e.target.dataset.startAt
+		});
+	  });
+	  closeButton.addEventListener('click', function () {
+		overlay.style.display = 'none';
+	  });
+	};
+
+	for (var i = 0; i < galleries.length; i++) {
+		imageGallerySetup();
+	}
+
+});
